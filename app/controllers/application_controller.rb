367baa80b2502aee7cf72ba7858ae238dcd5aacb
Francisco <francisco.jose1982@gmail.com>
2016-12-21 10:34:59 -0600

    saving application controller file after mergin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def current_user
+      if user.nil?
+        user = User.find(session[:user_id])
+      end
+      user
+    end
+
+  def require_login
+    redirect_to new_session_path if session[:user_id].nil?
+  end
 end
